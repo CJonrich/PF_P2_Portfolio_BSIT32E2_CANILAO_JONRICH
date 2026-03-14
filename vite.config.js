26e5ce7e15cc3ba485b23baf13b5ea45b6612bf3
CJonrich <c1707-23@itmlyceumalabang.onmicrosoft.com>
2026-03-14 22:03:56 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/PF_P2_Portfolio_BSIT32E2_GA_RONAN-JUSTINE/',
+  base: '/PF_P2_Portfolio_BSIT32E2_CANILAO_JONRICH/',
   plugins: [react()],
   build: {
     outDir: 'docs',
